1e704dc757d14800902809adaeda8cdb02b8aab4
Ken Micklas <kmicklas@gmail.com>
2013-04-26 14:50:41 -0400

    Add CardLayout for setup screens

--- src/terrace/gui/TerraceFrame.java
+++ src/terrace/gui/TerraceFrame.java
@@ -1,11 +1,22 @@
 package terrace.gui;
 
+import java.awt.CardLayout;
+
 import javax.swing.JFrame;
 
 public class TerraceFrame extends JFrame {
+	private static final String SETUP = "Setup";
+	private static final String GAME = "Game";
+	
+	CardLayout layout = new CardLayout();
+	
 	public TerraceFrame() {
-		add(new GamePanel());
 		setSize(1200, 1200);
+		setLayout(layout);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		add(new GamePanel(), GAME);
+		
+		layout.show(this, GAME);
 	}
 }
