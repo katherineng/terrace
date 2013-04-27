b8ac7655b7a3366502a8c4a30b4f4d61308b76f9
Winnie Wang <winnie_wang@brown.edu>
2013-04-27 18:33:22 -0400

    forgot to add files

---
+++ src/terrace/ai/TestMain.java
@@ -0,0 +1,12 @@
+package terrace.ai;
+
+import terrace.Variant;
+import terrace.gui.TerraceFrame;
+
+public class TestMain {
+
+	public static void main(String[] args){
+		TerraceFrame frame = new TerraceFrame(2, 8, Variant.STANDARD);
+		frame.setVisible(true);
+	}
+}
