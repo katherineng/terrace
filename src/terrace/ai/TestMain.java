9431a49f39356fee8b5042740f75a836b1b895a4
Ken Micklas <kmicklas@gmail.com>
2013-04-28 22:00:10 -0400

    Make AI test main compile

--- src/terrace/ai/TestMain.java
+++ src/terrace/ai/TestMain.java
@@ -1,11 +1,12 @@
 package terrace.ai;
 
 import terrace.Variant;
+import terrace.exception.IllegalMoveException;
 import terrace.gui.TerraceFrame;
 
 public class TestMain {
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws IllegalMoveException {
 		TerraceFrame frame = new TerraceFrame(1, 1, 8, Variant.STANDARD);
 		frame.setVisible(true);
 	}
