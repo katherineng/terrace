125efac53cef9ac634def08c31d0d3799620fd32
Katherine Ng <kwng@cs.brown.edu>
2013-05-01 12:29:45 -0400

    working on GUI

--- src/terrace/gui/TerraceFrame.java
+++ src/terrace/gui/TerraceFrame.java
@@ -1,8 +1,11 @@
 package terrace.gui;
 
 import java.awt.CardLayout;
+import java.awt.FlowLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import terrace.Game;
 import terrace.Variant;
@@ -21,5 +24,29 @@ public class TerraceFrame extends JFrame {
 		setLayout(layout);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		add(new GamePanel(new Game(numHuman, numAI, boardSize, variant)), GAME);
+	}
+	
+	private void createCards() {
+		JPanel mainMenu = new JPanel();
+		mainMenu.setLayout(new FlowLayout());
+		
+		mainMenu.add(new JButton("New Game"));
+		
+		mainMenu.add(new JButton("Create new game"));
+		mainMenu.add(new JButton("Join existing game"));
+		
+		JPanel players = new JPanel();
+		players.add(new JButton("1 Player"));
+		players.add(new JButton("2 Player"));
+		players.add(new JButton("3 Player"));
+		players.add(new JButton("4 Player"));
+		
+		JPanel newGame = new JPanel();
+		
+		JPanel networkCreate = new JPanel();
+		
+		JPanel networkJoin = new JPanel();
+		
+		JPanel local = new JPanel();
 	}
 }
