fd22d6ba6faa781af20fb70a278d9545160472cf
ww15 <wnnwng@gmail.com>
2013-05-01 14:15:40 -0400

    cleaned up ai code, got rid of case in minimax

--- src/terrace/ai/AI.java
+++ src/terrace/ai/AI.java
@@ -39,32 +39,11 @@ public class AI extends Player {
 	
 	@Override
 	public boolean makeMove() {
-//		List<Piece> pieces = getPieces();
-//		assert(pieces.size() > 0);
-//		LinkedList<Move> possibleMoves = new LinkedList<Move>();
-//		
-//		for (Piece piece: pieces) {
-//			for (Move move : _game.getBoard().getMoves(piece)){
-//				if (!possibleMoves.contains(move))
-//						possibleMoves.addLast(move);
-//			}
-//		}
-//		
-//		Move toMove = possibleMoves.get(getRandom(possibleMoves.size()));
-//		try {
-//			_game.movePiece(toMove.getPiece().getPosn(), toMove.getTo());
-//		} catch (IllegalMoveException e) {
-//			System.out.println(e.getMessage());
-//			e.printStackTrace();
-//		}
-		
-		
-		
 		SearchNode node;
 		try {
 			node = minimax(0, 0, _game.clone());
-			if (node.getMove() != null)
-				_game.movePiece(node.getMove().getPiece().getPosn(), node.getMove().getTo());
+			System.out.println(node.getMove());
+			_game.movePiece(node.getMove().getPiece().getPosn(), node.getMove().getTo());
 		} catch (CloneNotSupportedException | IllegalMoveException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -73,13 +52,6 @@ public class AI extends Player {
 		return true;
 	}
 	
-	/**
-	 * @param size - integer
-	 * @return a number between [0, size)
-	 */
-	private int getRandom(int size){
-		return (int)(Math.random() * ((size-1) + 1));
-	}
 	
 	//TODO: player's equals method shouldn't depend on pieces. make only dependent on color
 	// minimax(0, 3, game.clone())
@@ -87,27 +59,22 @@ public class AI extends Player {
 		assert(maxDepth % 2 == 0);
 		boolean maximizing = currDepth % 2 == 0;
 
-		System.out.println(maximizing);
 		//System.out.println(_game.getBoard().piecesToString());
-		if (gameState.isGameOver()){
-			return new SearchNode(null, estimateValue(gameState, this));
-		} else {
-			List<Move> possibleMoves = getPossibleMoves(gameState, gameState.getCurrentPlayer());
+		List<Move> possibleMoves = getPossibleMoves(gameState, gameState.getCurrentPlayer());
 
-			SearchNode bestNode = null;
-			double bestValue = (maximizing) ? Double.NEGATIVE_INFINITY : Double.POSITIVE_INFINITY;
-			for (Move m: possibleMoves){
-				DefaultBoardGame g = getGameState(m, gameState);
-				SearchNode currNode = (currDepth == maxDepth) ? new SearchNode(m, estimateValue(g, getPlayer(g))) : minimax(currDepth + 1, maxDepth, g);
-				if (maximizing &&currNode.getValue() > bestValue || // trying to maximize
-				!maximizing && currNode.getValue() < bestValue){ // trying to minimize
-					bestValue = currNode.getValue();
-					bestNode = currNode;
-				}
+		SearchNode bestNode = null;
+		double bestValue = (maximizing) ? Double.NEGATIVE_INFINITY : Double.POSITIVE_INFINITY;
+		for (Move m: possibleMoves){
+			DefaultBoardGame g = getGameState(m, gameState);
+			SearchNode currNode = (currDepth == maxDepth || g.isGameOver()) ? new SearchNode(m, estimateValue(g, getPlayer(g))) : minimax(currDepth + 1, maxDepth, g);
+			if (maximizing &&currNode.getValue() > bestValue || // trying to maximize
+					!maximizing && currNode.getValue() < bestValue){ // trying to minimize
+				bestValue = currNode.getValue();
+				bestNode = currNode;
 			}
-			return bestNode;
 		}
-		
+		return bestNode;
+
 	}
 
 	/**
@@ -120,7 +87,7 @@ public class AI extends Player {
 	 */
 	private DefaultBoardGame getGameState(Move m, DefaultBoardGame gameState) throws CloneNotSupportedException, IllegalMoveException{
 		DefaultBoardGame copy = gameState.clone();
-		copy.movePiece(m.getPiece().getPosn(), m.getTo());
+		copy.movePieceAI(m.getPiece().getPosn(), m.getTo());
 		return copy;
 	}
 	
@@ -159,7 +126,8 @@ public class AI extends Player {
 	
 
 	/**
-	 * Estimates the value of this game for the current player
+	 * Estimates the value of this game for the current player, by taking the difference
+	 * between the current player value and the opposing player's value
 	 * @return the value for this game for the current player
 	 */
 	public double estimateValue(DefaultBoardGame gameState, Player currPlayer){
@@ -181,19 +149,7 @@ public class AI extends Player {
 	/**
 	 * @param player - the Player whose value you're calculating
 	 * @return - a double indicating the value of this player's current position in the game.
-	 * Calculating the value of each piece by its size and its elevation.
-	 * The bigger the size/the higher the elevation, the better.
-	 * 
-	 * TODO: 
-	 * 
-	 * currently adding the two. maybe rethink that? Have 2 AIs play each other, one adding,
-	 * one not later on.
-	 * 
-	 * NEED TO REWRITE!
-	 * PROBLEM: big pieces in the beginning are encouraged to stay where they are if they are
-	 * at a higher elevation. In general, also need to take into account proximity to other pieces
-	 * + attack positions. But this might already be taken account for? Might have to search too deep
-	 * to get to this point though. grasd;flkjasd;lfkjasd;lfkj;
+	 * Calculating the value of each piece by its size.
 	 */
 	private double estimatePlayerValue(Player player){
 		double toRet = 0;
