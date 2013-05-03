b093af9623bcd219bb19f75ca1be7f1cc3713544
Katherine Ng <kwng@cs.brown.edu>
2013-05-02 21:37:02 -0400

    triangle board piece setup

--- src/terrace/TriangleBoardTest.java
+++ src/terrace/TriangleBoardTest.java
@@ -13,14 +13,14 @@ public class TriangleBoardTest {
 		_board4x4 = new TriangleBoard(4);
 		_board4x4.setUp();
 		
-		String expected = "[ 0 2 4 6 ]\n" + 
-						  "[ 1 3 5 6 ]\n" +
-						  "[ 2 2 4 5 ]\n" +
-						  "[ 3 3 4 4 ]\n" +
-						  "[ 4 4 3 3 ]\n" +
-						  "[ 5 4 2 2 ]\n" +
+		String expected = "[ 6 4 2 0 ]\n" +
 						  "[ 6 5 3 1 ]\n" +
-						  "[ 6 4 2 0 ]\n";
+						  "[ 5 4 2 2 ]\n" +
+						  "[ 4 4 3 3 ]\n" +
+						  "[ 3 3 4 4 ]\n" +
+						  "[ 2 2 4 5 ]\n" +
+						  "[ 1 3 5 6 ]\n" +
+						  "[ 0 2 4 6 ]\n";
 
 		
 		String elevations = _board4x4.elevationsToString();
@@ -32,12 +32,12 @@ public class TriangleBoardTest {
 		_board3x3 = new TriangleBoard(3);
 		_board3x3.setUp();
 		
-		String expected = "[ 0 2 4 ]\n" +
-						  "[ 1 3 4 ]\n" +
-						  "[ 2 2 3 ]\n" +
-						  "[ 3 2 2 ]\n" +
+		String expected = "[ 4 2 0 ]\n" +
 						  "[ 4 3 1 ]\n" +
-						  "[ 4 2 0 ]\n";
+						  "[ 3 2 2 ]\n" +
+						  "[ 2 2 3 ]\n" +
+						  "[ 1 3 4 ]\n" +
+						  "[ 0 2 4 ]\n";
 		
 		String elevations = _board3x3.elevationsToString();
 		assertEquals(expected, elevations);
