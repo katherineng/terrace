feae80064c04b087ee3f06415f5962c7e31f4a31
Ken Micklas <kmicklas@gmail.com>
2013-05-04 17:33:14 -0400

    Make GameServer Closeable

--- src/terrace/GameServer.java
+++ src/terrace/GameServer.java
@@ -1,5 +1,7 @@
 package terrace;
 
+import java.io.Closeable;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -8,31 +10,38 @@ import terrace.util.Callback;
 
 import com.google.common.base.Optional;
 
-public class GameServer {
+public class GameServer implements Closeable {
 	private final List<Callback<GameState>> _updateStateCBs = new LinkedList<>();
 	private final List<Callback<Player>> _notifyWinnerCBs = new LinkedList<>();
 	private final List<Callback<Player>> _notifyLoserCBs = new LinkedList<>();
+	
 	private GameState _game;
+	private boolean _closed = false;
 	
 	public GameServer(
 			GameState state
 	) {
 		_game = state;
 	}
+	
 	public GameState getState() {
 		return _game;
 	}
+	
 	public void addUpdateStateCB(Callback<GameState> cb) {
 		_updateStateCBs.add(cb);
 	}
+	
 	public void addWinnerCB(Callback<Player> cb) {
 		_notifyWinnerCBs.add(cb);
 	}
+	
 	public void addLoserCB(Callback<Player> cb) {
 		_notifyLoserCBs.add(cb);
 	}
+	
 	public void run() {
-		while (true) {
+		while (!_closed) {
 			Optional<Move> move = _game.getActivePlayer().getMove(45);
 			if (move.isPresent()) {
 				try {
@@ -68,5 +77,10 @@ public class GameServer {
 			
 			if (_game.getWinner().isPresent()) return;
 		}
+	}
+	
+	@Override
+	public void close() throws IOException {
+		_closed = true;
 	}
 }
