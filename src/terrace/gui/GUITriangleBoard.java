016b9c368e2582e825619dfc7efcd4d288a23714
Ken Micklas <kmicklas@gmail.com>
2013-05-05 02:15:07 -0400

    Synchronize updates to GUI pieces (fixes ConcurrentModificationExceptions)

--- src/terrace/gui/GUITriangleBoard.java
+++ src/terrace/gui/GUITriangleBoard.java
@@ -67,16 +67,20 @@ public class GUITriangleBoard extends GUIBoard {
 	}
 	
 	@Override
-	public void resetPieces() {	
-		_gamePieces.clear();
-		//needed because translation is relative to center of shape, not the corner
-		for (int row = 0; row < _panel._game.getBoard().getWidth(); row++)
-			for (int col = 0; col < _panel._game.getBoard().getHeight(); col++){
-				// set up _gamePiece
-				Piece piece = _panel._game.getBoard().getPieceAt(new Posn(row,  col));
-				if (piece != null) {
-					_gamePieces.add(new TriangleGamePiece(this, piece));
+	public void resetPieces() {
+		synchronized (_gamePieces) {
+			_gamePieces.clear();
+			
+			//needed because translation is relative to center of shape, not the corner
+			for (int row = 0; row < _panel._game.getBoard().getWidth(); row++) {
+				for (int col = 0; col < _panel._game.getBoard().getHeight(); col++){
+					// set up _gamePiece
+					Piece piece = _panel._game.getBoard().getPieceAt(new Posn(row,  col));
+					if (piece != null) {
+						_gamePieces.add(new TriangleGamePiece(this, piece));
+					}
 				}
 			}
+		}
 	}
 }
