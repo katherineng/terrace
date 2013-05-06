8f5c224939c7ac7d888459a2feeffcd9fbea2095
Ken Micklas <kmicklas@gmail.com>
2013-05-06 00:41:56 -0400

    Start reading moves in ClientConnection

--- src/terrace/network/ClientConnection.java
+++ src/terrace/network/ClientConnection.java
@@ -9,10 +9,15 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import terrace.util.Callback;
+import terrace.util.Posn;
 
 public class ClientConnection implements Closeable, Runnable {
+	private static final Pattern MOVE = Pattern.compile("([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+)");
+	
 	private final Socket _conn;
 	private final BufferedReader _in;
 	private final PrintWriter _out;
@@ -59,8 +64,10 @@ public class ClientConnection implements Closeable, Runnable {
 			}
 			
 			readNames();
-			
 			_onReady.call(this);
+			
+			mainLoop();
+			
 		} catch (IOException e) {
 			 System.err.println("LOG: " + e.getLocalizedMessage());
 		} finally {
@@ -74,7 +81,7 @@ public class ClientConnection implements Closeable, Runnable {
 		}
 	}
 	
-	public void readNames() throws IOException {
+	private void readNames() throws IOException {
 		while (true) {
 			String line = _in.readLine();
 			
@@ -84,6 +91,29 @@ public class ClientConnection implements Closeable, Runnable {
 				return;
 			} else {
 				_names.add(line);
+			}
+		}
+	}
+	
+	private void mainLoop() throws IOException {
+		while (true) {
+			String line = _in.readLine();
+			
+			if (_in == null) {
+				throw new EOFException("Client connection closed");
+			} else {
+				Matcher m = MOVE.matcher(line);
+				
+				if (m.matches()) {
+					int stateNum = Integer.parseInt(m.group(1));
+					
+					Posn from = new Posn(Integer.parseInt(m.group(2)), Integer.parseInt(m.group(3)));
+					Posn to = new Posn(Integer.parseInt(m.group(4)), Integer.parseInt(m.group(5)));
+					
+					
+				} else {
+					// TODO
+				}
 			}
 		}
 	}
