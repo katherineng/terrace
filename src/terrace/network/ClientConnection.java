578639804959950bc98973030973a29f199de18e
Ken Micklas <kmicklas@gmail.com>
2013-05-05 23:43:27 -0400

    Move name reading to it's own method in ClientConnection

--- src/terrace/network/ClientConnection.java
+++ src/terrace/network/ClientConnection.java
@@ -2,6 +2,7 @@ package terrace.network;
 
 import java.io.BufferedReader;
 import java.io.Closeable;
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -13,6 +14,9 @@ import terrace.util.Callback;
 
 public class ClientConnection implements Closeable, Runnable {
 	private final Socket _conn;
+	private final BufferedReader _in;
+	private final PrintWriter _out;
+	
 	private final Callback<ClientConnection> _onReady;
 	private final Callback<ClientConnection> _onDrop;
 	
@@ -22,8 +26,10 @@ public class ClientConnection implements Closeable, Runnable {
 			Socket conn,
 			Callback<ClientConnection> onReady,
 			Callback<ClientConnection> onDrop
-	) {
+	) throws IOException {
 		_conn = conn;
+		_in = new BufferedReader(new InputStreamReader(_conn.getInputStream()));
+		_out = new PrintWriter(_conn.getOutputStream(), true);
 		_onReady = onReady;
 		_onDrop = onDrop;
 	}
@@ -45,34 +51,40 @@ public class ClientConnection implements Closeable, Runnable {
 	@Override
 	public void run() {
 		try {
-			PrintWriter out = new PrintWriter(_conn.getOutputStream(), true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(_conn.getInputStream()));
+			_out.println("TERRACE-Server");
 			
-			out.println("TERRACE-Server");
-			
-			if (!"TERRACE-Client".equals(in.readLine())) {
+			if (!"TERRACE-Client".equals(_in.readLine())) {
 				System.err.println("LOG: Bad client. Dropping.");
 				return;
 			}
 			
-			while (true) {
-				String line = in.readLine();
-				
-				if (line == null) {
-					return;
-				} else if (line.equals("")) {
-					break;
-				} else {
-					_names.add(line);
-				}
-			}
+			readNames();
+			
 			_onReady.call(this);
-			
-			
 		} catch (IOException e) {
 			 System.err.println("LOG: " + e.getLocalizedMessage());
 		} finally {
 			_onDrop.call(this);
+			
+			try {
+				close();
+			} catch (IOException e) {
+				System.err.println("LOG: " + e.getLocalizedMessage());
+			}
+		}
+	}
+	
+	public void readNames() throws IOException {
+		while (true) {
+			String line = _in.readLine();
+			
+			if (line == null) {
+				throw new EOFException("Client connection closed");
+			} else if (line.equals("")) {
+				return;
+			} else {
+				_names.add(line);
+			}
 		}
 	}
 	
