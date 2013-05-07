e5c31d7f8b261cc868f9e6dd27d460f4b9097a5f
Ken Micklas <kmicklas@gmail.com>
2013-05-07 16:32:14 -0400

    Clean up GamePiece classes slightly

--- src/terrace/gui/game/SquareGamePiece.java
+++ src/terrace/gui/game/SquareGamePiece.java
@@ -1,6 +1,5 @@
 package terrace.gui.game;
 
-
 import javax.media.opengl.GL;
 import javax.media.opengl.GL2;
 import javax.media.opengl.glu.GLU;
@@ -12,14 +11,12 @@ import terrace.util.Posn;
 import terrace.util.Vector3d;
 
 public class SquareGamePiece extends GamePiece implements Drawable {
-	
 	public SquareGamePiece(GUIBoard board, Piece piece){
 		super(board, piece);
-
 	}
-
+	
 	@Override
-	public void drawPiece(GL2 gl) {
+	public void draw(GL2 gl) {
 		GLU glu = new GLU();
 		GLUquadric quadric = glu.gluNewQuadric();
 		
@@ -31,17 +28,17 @@ public class SquareGamePiece extends GamePiece implements Drawable {
 		double rowShift = 1.0/_board.getHeight()*pos.y;
 		double colShift = 1.0/_board.getWidth()*pos.x;
 		gl.glTranslated(.5 - shiftFactor - rowShift, _board.getElevation(pos) + _radius, .5 - shiftFactor - colShift);
-
+		
 		double mult = (_piece instanceof TPiece) ? .5 : 1;
 		Vector3d vec = _board._playerColors.get(_piece.getColor()); // set color
-		if (_selected) 
-			gl.glColor3d(.2*vec.x, .2*vec.y, .2*vec.z);
-		else
-			gl.glColor3d(mult*vec.x, mult*vec.y, vec.z);
 		
+		if (_selected) {
+			gl.glColor3d(.2*vec.x, .2*vec.y, .2*vec.z);
+		} else {
+			gl.glColor3d(mult*vec.x, mult*vec.y, vec.z);
+		}
 		
 		glu.gluSphere(quadric, _radius, 25, 25);
 	    gl.glPopMatrix();
 	}
-
 }
