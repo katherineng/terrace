4dea6e4c87921fd6311d35c1c8f5382347fd380d
Ken Micklas <kmicklas@gmail.com>
2013-05-07 16:33:57 -0400

    Close port in MessageTest

--- src/terrace/message/MessageTest.java
+++ src/terrace/message/MessageTest.java
@@ -12,19 +12,20 @@ import com.google.common.base.Optional;
 public class MessageTest {
 	@Test
 	public void test() throws InterruptedException, IOException {
-		Port<Integer> port = new Port<>();
-		Channel<Integer> a = port.newChannel(), b = port.newChannel();
-		
-		a.send(1);
-		b.send(2);
-		a.send(3);
-		
-		assertEquals(1, port.get().intValue());
-		assertEquals(2, port.get().intValue());
-		assertEquals(3, port.get().intValue());
-		assertEquals(Optional.absent(), port.tryGet(100, TimeUnit.MILLISECONDS));
-		
-		b.send(4, 100, TimeUnit.MILLISECONDS);
-		assertEquals(Optional.of(4), port.tryGet(100, TimeUnit.MILLISECONDS));
+		try (Port<Integer> port = new Port<>()) {
+			Channel<Integer> a = port.newChannel(), b = port.newChannel();
+			
+			a.send(1);
+			b.send(2);
+			a.send(3);
+			
+			assertEquals(1, port.get().intValue());
+			assertEquals(2, port.get().intValue());
+			assertEquals(3, port.get().intValue());
+			assertEquals(Optional.absent(), port.tryGet(100, TimeUnit.MILLISECONDS));
+			
+			b.send(4, 100, TimeUnit.MILLISECONDS);
+			assertEquals(Optional.of(4), port.tryGet(100, TimeUnit.MILLISECONDS));
+		}
 	}
 }
